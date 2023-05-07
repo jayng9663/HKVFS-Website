49758abb8db8da9bf32a439be37710ca82c32ff9
Jay Ng <jayng9663@gmail.com>
2023-05-07 02:03:44 -0700

    Update
    Added width alert, and fixing some bug

--- script/scripts.js
+++ script/scripts.js
@@ -3,3 +3,10 @@ var overlay = document.getElementById("overlay");
 window.addEventListener('load', function(){
   overlay.style.display = 'none';
 })
+
+$(document).ready(function() {
+  var width = $(window).width();
+  if (width < 1000){
+    alert('Your screen width is too small, please make the browser screen wider.');
+  }
+});
